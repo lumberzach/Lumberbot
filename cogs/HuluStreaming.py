d57f97d65d51922761030490f7d1908a02b5c6ca
Zach <boxcarracer0110@gmail.com>
2020-07-19 03:25:30 -0700

    removed some wasted code

--- cogs/HuluStreaming.py
+++ cogs/HuluStreaming.py
@@ -18,17 +18,17 @@ class HuluStreaming(commands.Cog):
     async def hulu(self, ctx):
         await ctx.send("Launching hulu... please wait until prompted to search")
 
-        client = discord.Client()
-
         # loading webdrivers and opening the website
         options = Options()
         # Path to your chrome profile
         options.add_argument("user-data-dir=C:\\Users\\Boxca\\AppData\\Local\\Google\\Chrome\\profiletwo")
         driver = webdriver.Chrome(r"C:\Users\Boxca\Downloads\Drivers\chromedriver_win32 (1)\chromedriver.exe",
                                   options=options)
+
+
         driver.get('https://www.hulu.com/')
 
-        time.sleep(5)
+        time.sleep(1)
 
         # sign in
         try:
@@ -46,22 +46,30 @@ class HuluStreaming(commands.Cog):
 
             try:
                 # closing the modals
-                welcome = WebDriverWait(driver, 4).until(
-                    EC.element_to_be_clickable((By.CLASS_NAME, "WelcomeModal__cta"))
-                )
+                time.sleep(2)
+                welcome = driver.find_element_by_class_name("WelcomeModal__cta")
+
                 welcome.click()
 
-                time.sleep(2)
-                await ctx.send("Ready to search hulu")
+                time.sleep(1)
+                await ctx.send(".")
+                await ctx.send("..")
+                await ctx.send("...")
 
             except NoSuchElementException:
-                await ctx.send("Ready to search hulu")
+                await ctx.send(".")
+                await ctx.send("..")
+                await ctx.send("...")
 
-            await ctx.send("Ready to search hulu")
+            await ctx.send(".")
+            await ctx.send("..")
+            await ctx.send("...")
         except NoSuchElementException:
-            await ctx.send("type /search to search")
+            await ctx.send(".")
+            await ctx.send("..")
+            await ctx.send("...")
 
-        time.sleep(2)
+        time.sleep(1)
 
 
 
@@ -77,7 +85,7 @@ class HuluStreaming(commands.Cog):
 
         search_box.send_keys(search_query)
 
-        time.sleep(5)
+        time.sleep(1)
         search_result_count = len(driver.find_elements_by_css_selector('li.InstantSearch__Option a'))
         await ctx.send(
             f"Please enter choice from 1 to {search_result_count}:") if search_result_count else print(
@@ -87,6 +95,9 @@ class HuluStreaming(commands.Cog):
                                       start=1):
 
             await ctx.send(f"Enter {num} for {element.text}")
+            if num == 3:
+                break
+
         msg = await self.bot.wait_for('message')
 
         choice = int(msg.content)
@@ -96,25 +107,67 @@ class HuluStreaming(commands.Cog):
         javaScript = f"document.querySelectorAll('li.InstantSearch__Option a')[{choice - 1}].click();"
         driver.execute_script(javaScript)
 
-        # playing the select option
-        try:
-            play_button = WebDriverWait(driver, 15).until(
-                EC.presence_of_element_located((By.CSS_SELECTOR, "div.WatchAction a"))
+        season_dropdown = None
+
+        def get_seasons():
+            global season_dropdown
+            season_dropdown = WebDriverWait(driver, 2).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, "button.Select__control"))
             )
+            season_dropdown.click()
 
-            play_button.click()
+            return WebDriverWait(driver, 5).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "li.Select__option")))
 
 
 
 
-        except:
-            await ctx.send("This video is restricted")
 
-        finally:
-            time.sleep(10)
 
-        #performing searc
 
+        seasons_list = get_seasons()
+        seasons_count = len(seasons_list)
+        await ctx.send(f"Please enter choice from 1 to {seasons_count}:")
+
+        for num, element in enumerate(seasons_list, start=1):
+            await ctx.send(f"Enter {num} for {element.text}")
+
+        msg = await self.bot.wait_for('message')
+
+        choice = int(msg.content)
+
+        if seasons_count < choice:
+            await ctx.send(f"Please enter only digits from 1 to {seasons_count}")
+
+        time.sleep(1)
+
+        driver.find_elements_by_css_selector('.Select__menu-list li')[choice - 1].click()
+
+
+
+
+        def get_episodes():
+            global episodes_exist
+            episodes_exist = WebDriverWait(driver, 2).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, "button.HoverSetup__button")))
+            return WebDriverWait(driver, 5).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "button.HoverSetup__button")))
+
+
+        episode_list = get_episodes()
+        episode_count = len(episode_list)
+
+
+        await ctx.send(f"Please enter choice from 1 to {episode_count}:")
+
+        for num, element in enumerate(episode_list, start=1):
+            await ctx.send(f"Enter {num} for {element.text}")
+
+        msg = await self.bot.wait_for('message')
+        choice = int(msg.content)
+
+        if episode_count < choice:
+            await ctx.send(f"Please enter only digits from 1 to {episode_count}")
+
+        driver.find_elements_by_css_selector("button.HoverSetup__button")[choice - 1].click()
 
 
 
