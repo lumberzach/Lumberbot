285c01898ceb20d9d983937b7cfcccea2d5a1a90
lumberzach <boxcarracer0110@gmail.com>
2021-07-26 17:38:59 -0700

    gitignore fix

--- chromeoptionstest.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
-options = Options()
-options.add_argument("user-data-dir=C:\\Users\\Boxca\\AppData\\Local\\Google\\Chrome\\User Data") #Path to your chrome profile
-driver = webdriver.Chrome(r"C:\Users\Boxca\Downloads\Drivers\chromedriver_win32 (1)\chromedriver.exe", options=options)
-
-driver.get('https://www.hulu.com/')
